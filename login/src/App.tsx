87c6b32cd41d7e69738a084af398afec33d9cbf6
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-11-10 10:33:30 +0530

    initial commit

---
+++ login/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Login from "./components/login";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
